c8c02d545fdb090bfe9aa452cb4cddd037b3f681
LuanGCMenezes <luangui2006@gmail.com>
2025-06-21 14:05:27 -0300

    Arrumando array

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
-﻿Console.WriteLine("--Aplicação: sorteando valores em uma lista");
-string[] nomes = ["O Zumbi", "O Assassino", "O Psicopata", "O Alienígena", "O Carniceiro", "O Matador",
- "O Vampiro", "O Maluco", "O Vingador", "O Monstro", "O Bruxo", "O Demônio"];
-string[] sobrenomes = ["Endiabrado", "Radioativo", "Endemoniado", "Vermelho", "Possuído", "Macabro", "Sombrio", "Sem Cabeça",
+﻿Console.Clear();
+Console.WriteLine("--Aplicação: sorteando valores em uma lista");
+string[] mesMonstro = {"O Zumbi", "O Assassino", "O Psicopata", "O Alienígena", "O Carniceiro", "O Matador",
+ "O Vampiro", "O Maluco", "O Vingador", "O Monstro", "O Bruxo", "O Demônio" };
+string[] diaMonstro = {"Endiabrado", "Radioativo", "Endemoniado", "Vermelho", "Possuído", "Macabro", "Sombrio", "Sem Cabeça",
 "Desconhecido", "Inconsciente", "Oculto", "Esquecido", "Lúgubre", "Invocado", "Caído", "Costurado", "Amaldiçoado", "Anormal",
 "Perturbado", "Sanguinário", "Tenebroso", "Imortal", "Ressuscitado", "do Pântano", "da Encruzilhada", "do Porão",
-"do Velho Poço", "sem Perdão", "do Cemitério", "da Meia-Noite", "da Lua Cheia"];
+"do Velho Poço", "sem Perdão", "do Cemitério", "da Meia-Noite", "da Lua Cheia" };
 
 
 while (true)
 {
-    Console.Write("Qual é o dia de nascimento da vítima (1 a 31) ");
-    int dia = Convert.ToInt32(Console.ReadLine());
-
     Console.Write("Qual é o mês de nascimento da vítima (1 a 12) ");
     int mes = Convert.ToInt32(Console.ReadLine());
 
+    Console.Write("Qual é o dia de nascimento da vítima (1 a 31) ");
+    int dia = Convert.ToInt32(Console.ReadLine());
+
     if (dia >= 1 && dia <= 31 && mes >= 1 && mes <= 12)
     {
-        string nomeCompleto = $"{nomes[mes - 1]} {sobrenomes[dia - 1]}";
+        string nomeCompleto = $"{mesMonstro[mes - 1]} {diaMonstro[dia - 1]}";
         Console.WriteLine($"Fuja! Lá vem {nomeCompleto}!");
         break;
     }
@@ -25,7 +26,4 @@ while (true)
     {
         Console.WriteLine("Digite uma data válida por gentileza: ");
     }
-
-
-
 }
